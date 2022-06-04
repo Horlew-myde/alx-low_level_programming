f50ced8d03adbe6abbfd3ad85febbd0ae3a3373e
Horlew-myde <mezamune@gmail.com>
2022-06-04 08:37:25 -0700

    advanced question 6

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
- * Return: Always 1
+ *
+ * Return: Always 0 (success)
  *
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\" -Dora Kopar, 2015-10-19\n", 60);
-	return(0);
+	char string[] = ("and that piece of art is useful\" -Dora Kopar, 2015-10-19\n");
+	write(1, string, 60);
+
+	return (1);
 }
