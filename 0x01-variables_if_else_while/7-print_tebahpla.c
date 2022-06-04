a634f12e9f2fb785654a54a3a8486e3da47c17c0
Horlew-myde <mezamune@gmail.com>
2022-06-04 15:52:47 -0700

    numbers 8 and 9

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -  Reversed alphabets printing with  Putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char rverse_alphabet = 'z';
+
+	while (reverse_alphabet >= 'a')
+	{
+		putchar(reverse_alphabet);
+		alpha--;
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - char number combo
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x= '0'; x < '10'; i++ )
+	{
+		putchar(x);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
