6c2f319de4b3d609e6184e4862d1a47b5b77f2b9
Horlew-myde <mezamune@gmail.com>
2022-06-04 08:17:20 -0700

    advanced question 6

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+include <unstd.h>
+/**
+ * main - Entry point
+ * Return: Always 1
+ *
+ */
+int main(void)
+{
+	Write(1, "and that piece of art is useful\" -Dora Kopar, 2015-10-19\n", 60);
+	return(0)
