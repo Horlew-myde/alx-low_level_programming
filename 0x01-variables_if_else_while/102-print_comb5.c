7ba6d4484c9de83b3d90e5ca8b15b73d8e313f55
Horlew-myde <mezamune@gmail.com>
2022-06-05 15:03:59 -0700

    advanced question

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num2 = num1 + 1 <= 00; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
@@ -19,8 +19,8 @@ int main(void)
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num1 ++ 98 && num2 ++ 99)
-				continue:
+			if (num1 == 98 && num2 == 99)
+				continue;
 
 			putchar(',');
 			putchar(' ');
