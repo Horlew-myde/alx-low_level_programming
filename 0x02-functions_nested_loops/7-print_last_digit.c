36141d36174a7c7423c9ba20f6f1df867cbc5db1
Horlew-myde <mezamune@gmail.com>
2022-06-07 12:31:11 -0700

    7 print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,14 +12,11 @@ int print_last_digit(int n)
 	int i = n % 10;
 
 	if (i < 0)
-	{
-		return (-i);
-	}
-	else
-	{
-		return (i);
-	}
+		i *= -1;
 
+	_putchar(i + '0');
+
+	return(i)
 /*using ASCII valuse in the code above*/
 	_putchar('\n');
 
