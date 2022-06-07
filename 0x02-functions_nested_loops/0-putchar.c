71ce76d987c470ac16931f9ecb34ab4e82e8929e
Horlew-myde <mezamune@gmail.com>
2022-06-07 03:55:56 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print _putchar thrn a new line
+ *
+ * the program should also return 0
+ *
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
