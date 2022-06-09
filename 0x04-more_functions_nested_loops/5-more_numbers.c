5bd784a1f21804bf1b6f21b67a032c5db21e28a6
Horlew-myde <mezamune@gmail.com>
2022-06-09 09:36:33 -0700

    10 times of 1-14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,14 @@ void more_numbers(void)
 {
 	int a, b;
 
-
 	for (a = 0; a < 10; a++)
 	{
-	for (b = 0; b < 15; b++)
-	{
-	if (b >= 10)
-	_putchar((b / 10) + 48);
-	_putchar((b % 10) + 48);
-	}
-	_putchar('\n');
+		for (b = 0; b < 15; b++)
+		{
+			if (b >= 10)
+				_putchar((b / 10) + 48);
+			_putchar((b % 10) + 48);
+		}
+		_putchar('\n');
 	}
 }
