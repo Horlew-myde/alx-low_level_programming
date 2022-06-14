5986502b4d00e71fa15a534df848e10d1db40fd4
Horlew-myde <mezamune@gmail.com>
2022-06-14 15:08:04 -0700

    key gen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -7,18 +7,20 @@
  */
 int main(void)
 {
-int key = 0, gen = 0;
-time_t t;
+	int key = 0, gen = 0;
+	time_t t;
 
-srand((unsigned int) time(&t));
-while (gen < 2772)
-{
-	key = rand() % 128;
-	if ((gen + key) > 2772)
-		break;
-	gen = gen + key;
-	printf("%c", key);
-}
-printf("%c\n", (2772 - gen));
-return (0);
+	srand((unsigned int) time(&t));
+	while (gen < 2772)
+	{
+
+		key = rand() % 128;
+		if ((gen + key) > 2772)
+			break;
+		gen = gen + key;
+	
+		printf("%c", key);
+	}
+	printf("%c\n", (2772 - gen));
+	return (0);
 }
