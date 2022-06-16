4a55f3262e4d1bccadf042e74af78c1652430831
Horlew-myde <mezamune@gmail.com>
2022-06-16 09:09:05 -0700

    using alais command to push

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase to uppercase.
+ * @str: The string to be changed.
+ *
+ * Return: A pointer to the changed string.
+ */
+char *string_toupper(char *str)
+{
+int string = 0;
+
+while (str[string])
+{
+	if (str[string] >= 'a' && str[string] <= 'z')
+		str[string] -= 32;
+
+	string++;
+}
+
+return (str);
+}
