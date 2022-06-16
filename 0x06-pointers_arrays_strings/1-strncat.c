94d6bfb9f4c92b3b99749653150ec67a805398f0
Horlew-myde <mezamune@gmail.com>
2022-06-15 17:30:12 -0700

    1 -string concatenator, 2- string copycat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,15 +11,15 @@ char *_strncat(char *dest, char *src)
 {
 	int str1 = 0, str2 = 0;
 
-	while (*(dest + str1) != '\0')
+	while (dest + str1) != '\0'
 	{
 		str1++;
 	}
 
 	while (str2 >= 0)
 	{
-		*(dest + str1) = *(src + str2);
-		if (*(src + str2) == '\0')
+		(dest + str1) = (src + str2);
+		if (src + str2) == '\0'
 			break;
 		str1++;
 		str2++;
