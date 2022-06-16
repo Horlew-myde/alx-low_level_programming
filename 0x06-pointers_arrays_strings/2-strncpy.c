a72ae78b4528f76781da3f0bcda459471c46a4d5
Horlew-myde <mezamune@gmail.com>
2022-06-16 05:03:53 -0700

    1 -string concatenator, 2- string copycat

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,10 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int str;
 
-	for (str = 0; str < n && src[i] != '\0'; str++)
-		dest[i] = src[i];
+	for (str = 0; str < n && src[str] != '\0'; str++)
+		dest[str] = src[str];
 	for ( ; str < n; str++)
-		dest[i] = '\0';
+		dest[str] = '\0';
 
 	return (dest);
 }
