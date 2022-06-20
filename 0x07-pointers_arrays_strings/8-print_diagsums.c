40c4543ce374d6b884b3e56be6e44a136fe33157
Horlew-myde <mezamune@gmail.com>
2022-06-20 07:46:23 -0700

    more on Pointers 007

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - prints sum of two diagonals of a square 
+ * print_diagsums - prints sum of two diagonals of a square
  * matrix of integers
  * @a: input pointer.
  * @size: size of the matrix
