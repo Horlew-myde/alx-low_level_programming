15bd6a8e23a8a569f5e559aae7171914448f59a7
Horlew-myde <mezamune@gmail.com>
2022-07-18 21:33:56 -0400

    more on singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,23 @@
+ines (17 sloc) 331 Bytes
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a list.
- * @h: head of a list.
- *
- * Return: numbers of nodes.
+ * print_listint - prints all elements of a list
+ * @h: list
+ * Return: number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-size_t nnodes = 0;
+	size_t num_nodes = 0;
 
-while (h != NULL)
-{
-	printf("%d\n", h->n);
-	h = h->next;
-	nnodes++;
-}
-return (nnodes);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n); /* autofills 0 if no int value given */
+
+		num_nodes += 1;
+		h = h->next;
+	}
+
+	return (num_nodes);
 }
