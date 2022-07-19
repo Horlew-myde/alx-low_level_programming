0af7b6b0ec703c7e094df7eb37586b913261c101
Horlew-myde <mezamune@gmail.com>
2022-07-18 21:49:52 -0400

    more on singly linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,14 +9,13 @@ ines (17 sloc) 331 Bytes
 
 size_t print_listint(const listint_t *h)
 {
-size_t num_nodes = 0;
+	size_t num_nodes = 0;
 
-while (h != NULL)
-
-{
-	printf("%d\n", h->n);
-	h = h->next;
-	nnodes++;
-}
-return (nnodes);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
 }
